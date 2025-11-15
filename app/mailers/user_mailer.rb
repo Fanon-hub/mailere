87c23d8ae67c0ce02e16adee7d7e6fe181da432e
Fanon-hub <fanonwalker@gmail.com>
2025-11-15 18:46:21 +0300

    Active Storage migration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
-    def registration_complete(user)
-        @user = user
-        mail(to: @user.email, from: 'admin@example.com', subject: 'Registration Completed')
-    end
-end
+  def registration_complete(user)
+    @user = user
+    mail(to: @user.email, from: 'admin@example.com', subject: 'Registration completed')
+  end
+end 
